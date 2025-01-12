68bd271ab8bacc757d5f45effc1a70f95e9271be
sumondas2001 <sumonchandradas1010@gmail.com>
2025-01-12 18:18:03 +0600

    property details pages img and same text show

--- src/Shared/PropertyDetailsCard/PropertyDetailsCard.jsx
+++ src/Shared/PropertyDetailsCard/PropertyDetailsCard.jsx
@@ -2,43 +2,73 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { useParams } from "react-router-dom";
+import PagesTitle from "../PagesTitle/PagesTitle";
+import PagesCoverImg from "../PagesCoverImg/PagesCoverImg";
 
 const PropertyDetailsCard = () => {
-     // all Property load for data based and set state
-     const [allProperties, setAllProperties] = useState([]);
-     // single Property find for all data and set state
-     const [property, setProperty] = useState([]);
+     // property id
      const { id } = useParams();
 
-     const { propertyname, location, title, types, landArea, orientation, frontRoad, apartmentSize, residentialFloors, numberOfParking, numberOfElevator, handoverDate, img, gallery } = property;
+     // all Property load for data based and set state
+     const [allProperties, setAllProperties] = useState([]);
 
-     // property id
 
-     // single Property find for all data
-     useEffect(() => {
-          const propertyDetails = allProperties.find(property => property.id === parseInt(id));
-          setProperty(propertyDetails)
-     }, [allProperties, id])
 
      // all Property load for data based
      useEffect(() => {
           axios.get('/allProperty.json')
                .then(res => {
-                    setAllProperties(res.data);
+                    // single Property find for all data and set state
+                    const propertyDetails = res.data?.find(property => property.id === parseInt(id));
+                    setAllProperties(propertyDetails);
 
                })
                .catch(error => {
                     console.log(error);
                     toast.error('Property Details Not Load')
                })
-     }, [])
+     }, [id]);
+
+
+     const info = {
+          CoverImg: 'https://anwarlandmark.com/admin/uploads/product/anwar-landmark-rosetta/2050x1152/1729403553EMBsg_m.jpg',
+          title: allProperties?.propertyname,
+          description: allProperties?.location
+     }
      return (
           <div>
-               {
+               <PagesTitle title={'PROPERTY DETAILS'}></PagesTitle>
+               <PagesCoverImg info={info}></PagesCoverImg>
 
-               }
+
+               <div className="lg:max-w-[1200px] md:max-w-[700px] max-w-[360px]  mx-auto">
+                    <div className="flex justify-between">
+                         <div className="flex flex-col items-start justify-center space-y-4">
+                              <h1 className="text-lg font-bold">Project Name:  <span className="ml-2 text-lg font-normal">{allProperties?.propertyname}</span> </h1>
+                              <p className="text-lg font-bold">Location :  <span className="ml-2 text-lg font-normal">{allProperties?.location}</span> </p>
+                              <p className="text-lg font-bold">landArea :  <span className="ml-2 text-lg font-normal">{allProperties?.landArea}</span></p>
+                              <p className="text-lg font-bold">Title :  <span className="ml-2 text-lg font-normal"> {allProperties?.title}</span></p>
+                              <p className="text-lg font-bold">Size : <span className="ml-2 text-lg font-normal">{allProperties?.landArea}</span></p>
+                              <p className="text-lg font-bold">Orientation : <span className="ml-2 text-lg font-normal">{allProperties?.orientation}</span></p>
+                              <p className="text-lg font-bold">Front Road : <span className="ml-2 text-lg font-normal">{allProperties?.frontRoad}</span></p>
+                              <p className="text-lg font-bold">Car Parking : <span className="ml-2 text-lg font-normal">{allProperties?.numberOfParking}</span></p>
+                              <p className="text-lg font-bold">Elevator : <span className="ml-2 text-lg font-normal">{allProperties?.numberOfElevator}</span></p>
+                              <p className="text-lg font-bold">HandoverDate : <span className="ml-2 text-lg font-normal">{allProperties?.handoverDate}</span></p>
+                              <p className="text-lg font-bold">Residential Floors : <span className="ml-2 text-lg font-normal">{allProperties?.residentialFloors}</span></p>
+                              {/* <p>Apartment Size A : <span>{allProperties?.apartmentSize.A}</span></p>
+                              <p>Apartment Size B : <span>{allProperties?.apartmentSize.B}</span></p> */}
+                         </div>
+                         <div>
+                              <img className="object-cover" src={allProperties.img} alt="" />
+                         </div>
+
+                    </div>
+               </div>
           </div>
      );
 };
 
 export default PropertyDetailsCard;
+
+
+
---
+++ src/Shared/PropertyDetailsFeatures/PropertyDetailsFeatures.jsx
@@ -0,0 +1,12 @@
+
+const PropertyDetailsFeatures = () => {
+     return (
+          <div>
+               <div>
+                    <h1 className="text-3xl font-semibold text-primaryColor">FEATURES & AMENITIES</h1>
+               </div>
+          </div>
+     );
+};
+
+export default PropertyDetailsFeatures;
